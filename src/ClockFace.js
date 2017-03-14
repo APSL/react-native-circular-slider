f2c1858878726d261c091ce264fb789ab7e4f2aa
alvaro <me@alvaromb.com>
2017-03-14 17:12:18 +0100

    Updated slider to handle only one input

--- src/ClockFace.js
+++ src/ClockFace.js
@@ -1,57 +1,62 @@
-import React, { PureComponent, PropTypes } from 'react';
-import { G, Circle, Text, Line } from 'react-native-svg';
-import range from 'lodash.range';
+/* @flow */
 
+import React, { PureComponent, PropTypes } from 'react'
+import { G, Circle, Text, Line } from 'react-native-svg'
+import range from 'lodash.range'
 
 export default class ClockFace extends PureComponent {
 
   static propTypes = {
     r: PropTypes.number,
     stroke: PropTypes.string,
+    steps: PropTypes.number,
+    showClockLines: PropTypes.bool,
   }
 
-  render() {
-    const { r, stroke } = this.props;
-    const faceRadius = r - 5;
-    const textRadius = r - 26;
-
+  render () {
+    const { r, stroke, steps, showClockLines } = this.props
+    const faceRadius = r - 5
+    const textRadius = r - 26
+    const lines = steps * 4
     return (
       <G>
         {
-          range(48).map(i => {
-            const cos = Math.cos(2 * Math.PI / 48 * i);
-            const sin = Math.sin(2 * Math.PI / 48 * i);
-
+          range(lines).map(i => {
+            const cos = Math.cos((2 * Math.PI / lines * i) - (Math.PI / 2))
+            const sin = Math.sin((2 * Math.PI / lines * i) - (Math.PI / 2))
+            if (i % 4 !== 0 && showClockLines === false) {
+              return
+            }
             return (
               <Line
                 key={i}
-                stroke={stroke}
+                stroke={(i === 0) ? 'red' : stroke}
                 strokeWidth={i % 4 === 0 ? 3 : 1}
                 x1={cos * faceRadius}
                 y1={sin * faceRadius}
                 x2={cos * (faceRadius - 7)}
                 y2={sin * (faceRadius - 7)}
               />
-            );
+            )
           })
         }
-      <G transform={{translate: "0, -9"}}>
+        <G transform={{translate: "0, -9"}}>
           {
-            range(12).map((h, i) => (
+            range(steps).map((h, i) => (
               <Text
                 key={i}
                 fill={stroke}
                 fontSize="16"
                 textAnchor="middle"
-                x={textRadius * Math.cos(2 * Math.PI / 12 * i - Math.PI / 2 + Math.PI / 6)}
-                y={textRadius * Math.sin(2 * Math.PI / 12 * i - Math.PI / 2 + Math.PI / 6)}
-              >
+                style={{color: 'red'}}
+                x={textRadius * Math.cos((2 * Math.PI / steps * i) - (Math.PI / 2) + (Math.PI / (steps / 2)))}
+                y={textRadius * Math.sin((2 * Math.PI / steps * i) - (Math.PI / 2) + (Math.PI / (steps / 2)))}>
                 {h + 1}
               </Text>
             ))
           }
         </G>
       </G>
-    );
+    )
   }
 }
